9806941ceecd7248074eb5668007a3ea7c74990f
FernandoZuchi <nandozuchi07@gmail.com>
2024-07-29 22:05:44 -0300

    Subindo os dois projetos :D

---
+++ FormPage/Form/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LoginSignup from './components/LoginSignup'
+
+const App = () => {
+  return (
+    <div>
+      {/* Renderizando o componente LoginSignup*/}
+      <LoginSignup />
+    </div>
+  )
+}
+
+export default App
